1abe7148e3dde0aa51ba9a3de7c13ef3ad75c9e3
Mahendra Patel <mpatel468@hotmail.co.uk>
2023-06-14 22:13:37 +0530

    containerized the app

--- src/configs/redis.config.js
+++ src/configs/redis.config.js
@@ -1,9 +1,15 @@
 const env = process.env;
 
-const REDIS_URI = {
-    host: env.REDIS_HOST,
-    port: env.REDIS_PORT
-}
+// const REDIS_URI = {
+//     host: env.REDIS_HOST,
+//     port: env.REDIS_PORT
+// }
+
+const host = env.REDIS_HOST;
+const port = env.REDIS_PORT;
+
+const REDIS_URL = `redis://${host}:${port}`;
 
 
-module.exports = REDIS_URI;
+// module.exports = REDIS_URI;
+module.exports = REDIS_URL;
--- src/connections/redis.connection.js
+++ src/connections/redis.connection.js
@@ -1,11 +1,15 @@
 const redis = require('redis');
-const REDIS_URI = require('../configs/redis.config');
+const REDIS_URL = require('../configs/redis.config');
+const redisUrl = process.env.REDIS_URL;
 
 
 let redisClient;
 
 (async () => {
-    redisClient = redis.createClient(REDIS_URI.REDIS_HOST, REDIS_URI.REDIS_PORT);
+    // redisClient = redis.createClient(REDIS_URI.REDIS_HOST, REDIS_URI.REDIS_PORT);
+    redisClient = redis.createClient({
+        url: REDIS_URL
+    })
     redisClient.on("error", (error) => {
         console.log(`Error ${error}`);
     });
